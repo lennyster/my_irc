dfbdf7275185b5e9d0f7369261d1149907b4900a
unknown <edouardnoel.tardif@gmail.com>
2020-05-19 17:11:25 +0200

    create chan/join/delete/part w/o gestion erreur

--- back/server.js
+++ back/server.js
@@ -26,6 +26,7 @@ app.get("/", (req, res, next) => {
 
 let chatmessage = [];
 let rooms = [];
+rooms['users'] = { owner: null, createdat : Date.now(), users : []};
 
 let commandes = {
     nick : function (value,tab,socket){
@@ -48,6 +49,7 @@ let commandes = {
     },
     list : function (value,tab,socket){
         console.log('Check les channel contenant '+value);
+        console.log(rooms);
     },
     create : function (value,tab,socket){
         let created = false;
@@ -71,6 +73,8 @@ let commandes = {
             socket.leave(lastroom);
             rooms[room] = { owner: socket.id, createdat : Date.now(), users : []};
             rooms[room].users.push(Users[socket.id]);
+            removeA(rooms[lastroom].users, Users[socket.id]);
+
             // io.sockets.manager.roomClients[socket.id]
             io.emit('server','SERVER : '+Users[socket.id]+ ' creer le channel '+room);
             console.log(rooms);
@@ -83,20 +87,121 @@ let commandes = {
         }
     },
     delete : function (value,tab,socket){
-        console.log('supprime le channel s\'appelant '+value);
+        let room = tab.join(' ');
+        let created = false;
+        for(x in rooms){
+            if(x == room){
+                created = true;
+            }
+        }
+
+        if(created){
+            let lastroom;
+            for(x in socket.rooms){
+                if(x !== socket.id){
+                    lastroom = x;
+                }
+            }
+            if(rooms[lastroom].owner == socket.id){
+                var clients_in_the_room = io.sockets.adapter.rooms[lastroom].sockets; 
+                for (var clientId in clients_in_the_room ) {
+                // console.log('client: %s', clientId); //Seeing is believing 
+                var client_socket = io.sockets.connected[clientId];//Do whatever you want with this
+                client_socket.leave(lastroom);
+                client_socket.join('users');
+                client_socket.emit('server','SERVER : vous avez ete reconnecte au channel users');
+                rooms['users'].users.push(Users[client_socket.id]);
+                }
+                delete rooms[lastroom];
+                // let test = rooms.splice(lastroom,1);
+                io.emit('server','SERVER : '+Users[socket.id]+ ' delete le channel '+room);
+
+                console.log('Delete le channel '+value);
+            } else {
+                socket.emit('server','SERVER : le channel "'+value+'" ne vous appartient pas !');
+            }
+        } else {
+            socket.emit('server','SERVER : le channel "'+value+'" n\'existe pas');
+        }
     },
     join : function (value,tab,socket){
-        console.log('Rejoin le channel '+value);
+        let room = tab.join(' ');
+        let created = false;
+        for(x in rooms){
+            if(x == room){
+                created = true;
+            }
+        }
+
+        if(created){
+            let lastroom;
+            for(x in socket.rooms){
+                if(x !== socket.id){
+                    lastroom = x;
+                }
+            }
+            console.log(lastroom,socket.rooms);
+            
+            socket.join(room);
+            socket.leave(lastroom);
+            removeA(rooms[lastroom].users, Users[socket.id]);
+            rooms[room].users.push(Users[socket.id]);
+            // io.sockets.manager.roomClients[socket.id]
+            io.emit('server','SERVER : '+Users[socket.id]+ ' rejoint le channel '+room);
+            console.log(rooms);
+            socket.broadcast.to(room).emit('server','SERVER : '+Users[socket.id]+ ' vient de rejoindre le channel !');
+
+
+
+            // console.log('Creer un channel s\'appelant '+value);
+            console.log('Rejoint le channel '+value);
+        } else {
+            socket.emit('server','SERVER : le channel "'+value+'" n\'existe pas');
+        }
     },
     part : function (value,tab,socket){
-        console.log('Quitte le channel '+value);
+        let room = tab.join(' ');
+        let created = false;
+        for(x in rooms){
+            if(x == room){
+                created = true;
+            }
+        }
+
+        if(created){
+            let lastroom;
+            for(x in socket.rooms){
+                if(x !== socket.id){
+                    lastroom = x;
+                }
+            }
+            
+                
+                socket.leave(lastroom);
+                socket.join('users');
+                socket.emit('server','SERVER : vous avez ete reconnecte au channel users');
+                removeA(rooms[lastroom].users, Users[socket.id]);
+                rooms['users'].users.push(Users[socket.id]);
+                socket.broadcast.to(lastroom).emit('server','SERVER : '+Users[socket.id]+ ' vient de quitter le channel !');
+                socket.broadcast.to('users').emit('server','SERVER : '+Users[socket.id]+ ' vient de rejoindre le channel !');
+
+            
+        } else {
+            socket.emit('server','SERVER : le channel "'+value+'" n\'existe pas');
+        }
     },
     users : function (value,tab,socket){
-        let array = [];
-        for(let x in Users){
-            array.push(Users[x]);
+        let lastroom;
+        for(x in socket.rooms){
+            if(x !== socket.id){
+                lastroom = x;
+            }
         }
-        socket.emit('server',array);
+        let array = [];
+        for(let x in rooms[lastroom].users){
+            array.push(rooms[lastroom].users[x]);
+        }
+        socket.emit('server','SERVER : les personnes connectes sont : '+ array);
     },
     msg : function (value,tab,socket){
         let tab2 = tab;
@@ -119,8 +224,17 @@ let commandes = {
     },
 };
 
-
-
+// ENLEVE UNE VALUE DUN TABLEAU
+function removeA(arr) {
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax= arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
+    }
+    return arr;
+}
 
 
 
@@ -149,7 +263,9 @@ io.on('connection', socket => {
                 Users[socket.id] = inputUsername; 
                 //socket.broadcast.emit('chatmessage','Quelqu\'un s\'est connecte')
                 socket.broadcast.emit('server','SERVER :'+Users[socket.id] + ' s\'est connecte')
+                rooms['users'].users.push(Users[socket.id]);
             })
+
         }
     })
     socket.emit('message','Connection recu');
